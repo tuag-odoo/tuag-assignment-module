dd1c44883d711fd15a5086777ae93650e181e4c2
tuag-odoo <tuag@odoo.com>
2024-03-06 15:52:56 +0530

    [FIX] stock_transport: adapt changes given by mentor
    - Added validations to the compute method for both volume and weight.
    - Incorporated dependencies for volume and weight in the compute method.
    - Removed auto-install from the stock_transfer module.

--- stock_transport/models/inherit_batch_picking.py
+++ stock_transport/models/inherit_batch_picking.py
@@ -4,13 +4,13 @@ from odoo.exceptions import ValidationError
 class InheritBatchPicking(models.Model):
     _inherit = 'stock.picking.batch'
 
-    dock_id = fields.Many2one('stock.dock', string="Dock")
-    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', placeholder='Third party provider', store=True)
-    vehicle_category_id = fields.Many2one('fleet.vehicle.model.category', string='Vehicle Category', placeholder='eg. Semi-truck', store=True)
+    dock_id = fields.Many2one('stock.dock', string="Dock", placeholder="eg. dock 1")
+    vehicle_id = fields.Many2one('fleet.vehicle', string='Vehicle', store=True)
+    vehicle_category_id = fields.Many2one('fleet.vehicle.model.category', string='Vehicle Category', store=True)
     volume = fields.Float(compute="_compute_volume", store=True)
     weight = fields.Float(compute="_compute_weight", store=True)
     
-    @api.depends("vehicle_category_id.max_volume", "picking_ids")
+    @api.depends("vehicle_category_id.max_volume", "picking_ids", "picking_ids.move_ids_without_package.quantity", "picking_ids.move_ids_without_package.product_id.volume")
     def _compute_volume(self):
         for record in self:
             total_volume = 0
@@ -26,19 +26,19 @@ class InheritBatchPicking(models.Model):
         else:
             self.volume = 0
  
-    @api.depends("vehicle_category_id.max_weight","picking_ids")
+    @api.depends("vehicle_category_id.max_weight","picking_ids", "picking_ids.move_ids_without_package.quantity", "picking_ids.move_ids_without_package.product_id.weight")
     def _compute_weight(self):
         for record in self:
-            total_wight = 0
+            total_weight = 0
             for batch in record.picking_ids:
                 for product in batch.move_ids_without_package:
-                    total_wight += (product.product_id.weight)*(product.quantity)
+                    total_weight += (product.product_id.weight)*(product.quantity)
         if(record.vehicle_category_id.max_weight != 0):
-            weight_percentage = round(100 * (total_wight / record.vehicle_category_id.max_weight))
+            weight_percentage = round(100 * (total_weight / record.vehicle_category_id.max_weight))
             if(weight_percentage > 100):
                 raise ValidationError(f"""Your Product weight is greater then inventory capacity""")
             else:
-                self.volume = weight_percentage
+                self.weight = weight_percentage
         else:
             self.weight = 0
 
--- stock_transport/models/inherit_batch_picking_volume.py
+++ stock_transport/models/inherit_batch_picking_volume.py
@@ -5,9 +5,10 @@ class StockPicking(models.Model):
 
     volume = fields.Float('Volume', compute='_compute_volume')
 
+    @api.depends("move_ids.product_id", "move_ids.quantity")
     def _compute_volume(self):
         for record in self:
                 total = 0
                 for move in record.move_ids:
-                    total += move.product_id.volume*move.product_qty
+                    total += move.product_id.volume*move.quantity
                 record.volume = total
